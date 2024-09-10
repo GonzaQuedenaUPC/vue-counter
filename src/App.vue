2eafcb5411256d55cf92e46968f6f49eca5a0f34
GonzaQuedenaUPC <u202113656@upc.edu.pe>
2024-09-09 19:03:39 -0500

    feat: add javascript implementation and template for counter exercise.

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<script setup>
+  import {ref} from 'vue'
+
+  let counter = ref(0);
+  const reset = () => counter.value = 0;
+  const increment = () => counter.value++;
+  const decrement = () => counter.value--;
+  const counterLimit = () => {
+    if (counter.value < 0)
+      return 'red';
+    else if (counter.value > 0)
+      return 'green';
+
+    return '#000000';
+  }
+</script>
+
+<template>
+  <div class="container">
+    <div class="counter">
+
+      <h2 :style="{color: counterLimit()}" class="counter__label">
+        {{ counter }}
+      </h2>
+
+      <div class="counter__buttons">
+        <button v-on:click="increment" class="counter__button counter__button--increment">Increment</button>
+        <button v-on:click="reset" class="counter__button counter__button--reset">Reset</button>
+        <button v-on:click="decrement" class="counter__button counter__button--decrement">Decrement</button>
+      </div>
+
+    </div>
+  </div>
+</template>
+
+<style scoped>
+ 
+
+</style>
